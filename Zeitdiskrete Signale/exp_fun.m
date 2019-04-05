dff7c5d5742658637a57c4ad311953af92a2fd08
Daniel Hammer <braineniac@protonmail.com>
2019-04-05 02:21:42 +0200

    Add first script

---
+++ Zeitdiskrete Signale/exp_fun.m
@@ -0,0 +1,31 @@
+#!/usr/bin/env octave
+
+
+function main()
+    n_pos = 0:10;
+    n = -10:10;
+
+    plot_a2n(n_pos,0.5);
+    plot_a2absn(n,0.5);
+    plot_myexp(n,0.05);
+end
+
+function plot_a2n(n,a)
+    x_n = x_n = a.^n;
+    plot(n,x_n);
+end
+
+function plot_a2absn(n,a)
+    x_n=a.^abs(n);
+    plot(n,x_n);
+end
+
+function plot_myexp(n,alpha)
+    x_n = exp(-alpha*n.^2);
+    plot(n,x_n);
+end
+
+function plot(n,x)
+    plot = stem(n,x);
+    waitfor(plot);
+end
